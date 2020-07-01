2e6fffe8b896e654eadeb68b0feb1ff8551d1b71
aarnadlr <aaronadler@gmail.com>
2020-07-01 12:40:48 -0400

    Initial commit.

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>The Person Finder</h1>
+      <p>If you just can’t find someone and need to know what they look like, you’ve come to the right place! Just type the name of the person you are looking for below into the search box!</p>
+    </div>
+  );
+}
+
+export default App;
